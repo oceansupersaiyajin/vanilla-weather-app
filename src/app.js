d8cff6434b2ddd0031ad7d3361b91e3166653fb9
oceansupersaiyajin <damla.k@live.at>
2023-10-19 16:08:48 +0200

    created forecast function in JS

--- src/app.js
+++ src/app.js
@@ -27,6 +27,36 @@ let timestamp = new Date().getTime();
 let dateZero = document.querySelector("#date");
 dateZero.innerHTML = formatDate(timestamp);
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 //
 
 function displayTemperature(response) {
@@ -96,5 +126,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
 
 search("New York");
-
+displayForecast();
 //
